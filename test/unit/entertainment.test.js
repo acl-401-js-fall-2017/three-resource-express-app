db532f57f9007ef83b06241c4007cff4fd254bdf
ckrountree <jmail0207@gmail.com>
2017-10-19 11:26:36 -0700

    start actor model test

---
+++ test/unit/entertainment.test.js
@@ -0,0 +1,51 @@
+const { assert } = require('chai');
+const Actor = require('../lib/models/actor');
+// const Movie = require('../lib/models/movie');
+// const Studio = require('../lib/models/studio');
+
+describe('Actor Model', () => {
+    
+    it('good model', () => {
+        const actor = new Actor({
+            name: 'Ryan Gosling',
+            movies: ['La La Land', 'Notebook', 'Crazy, Stupid Love'],
+        });
+
+        assert.equal(Actor.validateSync(), undefined);
+    });
+
+    // it('Movies starred in', () => {
+    //     const actor = new Actor({ 
+    //         movies: [
+    //             { name: 'Notebook' },
+    //             { name: 'NOT NOTEPAD' }
+    //         ] 
+    //     });
+    //     const { errors } = actor.validateSync();
+    //     assert.equal(errors['movies.1.name'].kind, 'enum');
+    // });
+
+    // it('required field', () => {
+    //     const actor = new Actor({});
+    //     const { errors } = actor.validateSync();
+    //     assert.equal(errors['name'].kind, 'required');
+    // });
+});
+
+// describe('Movie Model', () => {
+
+//     it('good model', () => {
+//         const movie = new Movie({
+//             title: 'Shawshank Redemption',
+//             releaseYear: 1994,
+//         });
+
+//         assert.equal(movie.validateSync(), undefined);
+//     });
+
+//     it('required field', () => {
+//         const movie = new Movie({});
+//         const { errors } = movie.validateSync();
+//         assert.equal(errors['title'].kind, 'required');
+//     });
+// });
