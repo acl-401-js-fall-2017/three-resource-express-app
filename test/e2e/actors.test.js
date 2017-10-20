92f367af437cab55a1764165ebfa2e4b6f46e4b7
erdem koral <koral.erdem@gmail.com>
2017-10-19 21:36:55 -0700

    started redoing the project "No copy paste"

--- test/e2e/actors.test.js
+++ test/e2e/actors.test.js
@@ -2,61 +2,52 @@ const request = require('./request');
 const mongoose = require('mongoose');
 const assert = require('chai').assert;
 
-describe ('actors API', () => {
+describe('Actors Api', () => {
+    
     beforeEach(() => mongoose.connection.dropDatabase());
-
     const actor1 = {name: 'Popeye', gender: 'male'};
 
-    it('saves with id', () => {
+    it('saves actor with id', () => {
         return request.post('/api/actors')
             .send(actor1)
             .then(res => {
-                const savedActor = res.body;
-                assert.ok(savedActor._id);
-                assert.equal(savedActor.name, actor1.name);
+                assert.ok(res.body._id);
+                assert.equal(res.body.name, actor1.name);
             });
     });
 
-    it('does not save if there is validation error', () =>{
-        return request.post('/api/actors')
-            .send({})
-            .then(() => { throw new Error();},
-                err => {
-                    assert.equal(err.status, 400);
-                });
-    });
-
-    it('gets by id', () => {
-        let savedActor = null;
-        return request.post('/api/actors')
+    it('gets actor with an id', () => {
+        let savedActor =null;
+        return  request.post('/api/actors')
             .send(actor1)
             .then(res => {
                 savedActor = res.body;
                 return request.get(`/api/actors/${savedActor._id}`);
-            });
+            })
+            .then(res => {
+                assert.equal(res.body.name, actor1.name);
+            }); 
     });
 
     it('get by id return 404 for bad id', () => {
         return request.get('/api/actors/59dfeaeb083bf9beecc97ce8')
             .then(
-                ()=> {throw new Error();},
+                () => {throw new Error();},
                 err => {
                     assert.equal(err.status, 404);
-                }
-            );
+                });
     });
 
-    it('get all the actors', () => {
+    it('get all actors', () => {
         const actor2 = { name: 'Olive Oyl', gender: 'female'};
-        
-        const actorCollection = [actor1, actor2].map(actor => {
+        const actorCollection =[actor1, actor2].map(actor => {
             return request.post('/api/actors')
                 .send(actor)
                 .then(res => res.body);
         });
-        
+
         let saved = null;
-        return Promise.all(actorCollection) 
+        return Promise.all(actorCollection)
             .then(_saved => {
                 saved = _saved;
                 return request.get('/api/actors');
@@ -64,43 +55,37 @@ describe ('actors API', () => {
             .then(res => {
                 assert.deepEqual(res.body, saved);
             });
+
     });
 
-    it('deletes with id', () => {
-        let savedActor =null;
+    it('deletes with id',() => {
         return request.post('/api/actors')
             .send(actor1)
             .then(res => {
-                savedActor = res.body;
-                return request.delete(`/api/actors/${savedActor._id}`);
+                return request.delete(`/api/actors/${res.body._id}`);
             })
             .then(res => {
-                assert.deepEqual(res.body, { removed: true });
+                assert.deepEqual(res.body, {removed: true});
             });
-
     });
 
     it('returns false when deletes with bad id', () => {
-        return request.delete('/api/actors/59dfeaeb083bf9beecc97ce6')
+        return request.delete('/api/actors/59dfeaeb083bf9beecc97ce4')
             .then(res => {
-                assert.deepEqual(res.body, { removed: false });
+                assert.deepEqual(res.body, {removed:false});
             });
-
     });
 
-    it('changes a saved actor with id', () => {
-        let update = { name: 'Bluto' };
-        let savedActor =null;
+    it('changes saved actor with id', () => {
+        let update = { name: 'Bluto'};
         return request.post('/api/actors')
             .send(actor1)
             .then(res => {
-                savedActor = res.body;
-                return request.put(`/api/actors/${savedActor._id}`).send(update);
+                return request.put(`/api/actors/${res.body._id}`).send(update);
             })
             .then(res => {
-                assert.equal(res.body.name, update.name); 
-                    
+                assert.equal(res.body.name, update.name);
             });
-
     });
+    
 });
