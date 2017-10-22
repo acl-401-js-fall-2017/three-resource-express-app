fd5ecf86123b97e4a3ab1ace129f49762b86d503
Shane Moyo <smoyo@pdx.edu>
2017-10-21 18:41:54 -0700

    Added failing save reality show test

---
+++ test/e2e/realitytv.test.js
@@ -0,0 +1,29 @@
+const assert = require('chai').assert;
+const mongoose = require('mongoose');
+const request = require('./request');
+
+describe('Realitytv API', () => {
+
+    beforeEach(() => mongoose.connection.dropDatabase());
+
+    const realWorld = {
+        name: 'The Real World',
+        releaseYear: 1992,
+        cast: [
+            { name: 'Julie Gentry', season: 1 },
+            { name: 'Norman Korpi', season: 1 },
+            { name: 'Andre Comeau', season: 1 }
+        ]
+    }
+
+    it('Saves a reality show with id', ()=>{
+        return request.post('/api/realitytv')
+            .send(realWorld)
+            .then(res => {
+                const realityShow = res.body;
+                assert.ok(realityShow._id);
+                assert.equal(realityShow.name, realWorld.name);
+            });
+    });
+
+});
