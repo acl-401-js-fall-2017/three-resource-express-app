546aaeaf244238a0c0f619e42dbd9178b99d0857
Shane Moyo <smoyo@pdx.edu>
2017-10-21 17:59:16 -0700

    added failing get sitcom by id test

--- test/e2e/sitcom.test.js
+++ test/e2e/sitcom.test.js
@@ -7,20 +7,39 @@ describe('Sitcom API', () => {
     beforeEach(()=> mongoose.connection.dropDatabase());
 
     const freshPrince = {
-        name: 'The Fresh Prince of Bel-Air',
+        title: 'The Fresh Prince of Bel-Air',
         releaseYear: 1990,
         cast: [{actor: 'Will Smith', role: 'Will Smith'},
             {actor: 'James Avery ', role: 'Philip Banks'}]
     };
 
     it('Saves a sitcom with id', ()=>{
-        return request.post('/api/sitcom')
+        return request.post('/api/sitcoms')
             .send(freshPrince)
             .then(res => {
                 const sitcom = res.body;
                 assert.ok(sitcom._id);
                 assert.equal(sitcom.name, freshPrince.name);
             });
+    });
+
+    it('Shoud get a sitcom by id', ()=>{
+        let sitcom;
+        let id;
+
+        return request.post('/api/sitcoms')
+            .send(freshPrince)
+            .then(res => {
+                sitcom = res.body;
+                id = sitcom._id;
+            })
+            .then(()=>{
+                return request.get(`/api/sitcms/${id}`)
+                    .then(res =>{
+                        assert.deepEqual(res.body, sitcom);
+                    });
+            });
+
     });
 
 });
